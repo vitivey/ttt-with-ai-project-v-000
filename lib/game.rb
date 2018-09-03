55d4349deaaab2f9759e31b1e8b7bc64a130b58f
Zindzi Thompson <zindzi.thompson@gmail.com>
2018-09-03 23:47:44 +0000

    automatically backed up by learn

--- lib/game.rb
+++ lib/game.rb
@@ -1,6 +1,6 @@
 class Game
 
-  WIN_COMBINATIONS= = [
+  WIN_COMBINATIONS=[
       [0,1,2],
       [3,4,5],
       [6,7,8],
@@ -17,9 +17,9 @@ class Game
 
   def board
 
-  end  
+  end
 
-  
+
 
 
 end
