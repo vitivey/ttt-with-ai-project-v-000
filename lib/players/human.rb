d06d9987058fb7ba2338de2d7dd7fa227a370a16
Zindzi Thompson <zindzi.thompson@gmail.com>
2018-09-04 03:33:48 +0000

    automatically backed up by learn

--- lib/players/human.rb
+++ lib/players/human.rb
@@ -5,7 +5,7 @@ module Players
     class Human < Player
 
         def move(board)
-          puts "Enter move between 1-9"
+          puts "Enter move between 1-9:"
           value=gets
           # board.cells[value.to_i]
 
