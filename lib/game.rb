819c41fbd8b6f2ed30d14668ce5562cce67b7b59
Zindzi Thompson <zindzi.thompson@gmail.com>
2018-09-04 00:24:00 +0000

    automatically backed up by learn

--- lib/game.rb
+++ lib/game.rb
@@ -19,14 +19,6 @@ class Game
     self.board=board
   end
 
-  # def player_1=(token)
-  #   @player_1=Player.new(token)
-  # end
-  #
-  # def player_2=(token)
-  #   @player_2=Player.new(token)
-  # end
-
 
   def current_player
 
