aa82f9fbded506a08942ac977f960ace30a21a98
Zindzi Thompson <zindzi.thompson@gmail.com>
2018-09-04 04:21:29 +0000

    automatically backed up by learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -5,18 +5,18 @@ module Players
     class Computer < Player
 
       def move(board)
-        array = []
-        array_corners=[]
-        board.cells.each_with_index {|position, index| array << "#{index+1}" if position == " " }
-        array.each{|position| array_corners << position if position=="1" || position=="3" || position=="7" || position=="9"
+          array = []
+          array_corners=[]
+          board.cells.each_with_index {|position, index| array << "#{index+1}" if position == " " }
+          array.each{|position| array_corners << position if position=="1" || position=="3" || position=="7" || position=="9"
 
-        if array.include?("5")
-          "5"
-        elsif !array_corners.empty?
-          array_corners.sample
-        else
-          array.sample
-        end
+          if array.include?("5")
+            "5"
+          elsif !array_corners.empty?
+            array_corners.sample
+          else
+            array.sample
+          end
       end
 
     end
