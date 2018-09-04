ce38d10b8167a3df052ee955a4d01d37e5413f04
Zindzi Thompson <zindzi.thompson@gmail.com>
2018-09-04 01:18:58 +0000

    automatically backed up by learn

--- lib/game.rb
+++ lib/game.rb
@@ -36,7 +36,12 @@ class Game
       match_check=board.cells.values_at(win_option[0],win_option[1],win_option[2])
       combo=win_option if match_check.all?{|token| token=="X" || token =="O"}
     end
-    combo
+    
+    if combo != []
+      combo
+    else
+      false
+    end
   end
 
   def draw?
