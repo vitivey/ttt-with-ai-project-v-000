9ee13068d6ac82a673dcf08fb22ade78024518d7
Zindzi Thompson <zindzi.thompson@gmail.com>
2018-09-04 04:20:40 +0000

    automatically backed up by learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -6,14 +6,14 @@ module Players
 
       def move(board)
         array = []
-        array_priority=[]
+        array_corners=[]
         board.cells.each_with_index {|position, index| array << "#{index+1}" if position == " " }
-        
+        array.each{|position| array_corners << position if position=="1" || position=="3" || position=="7" || position=="9"
 
         if array.include?("5")
           "5"
-        elsif !array_priority.empty?
-          array_priority.sample
+        elsif !array_corners.empty?
+          array_corners.sample
         else
           array.sample
         end
