1e60a657651552e01a8801afb21d6a511da02944
Zindzi Thompson <zindzi.thompson@gmail.com>
2018-09-04 00:53:58 +0000

    automatically backed up by learn

--- lib/game.rb
+++ lib/game.rb
@@ -29,10 +29,9 @@ class Game
   end
 
   def won?
-    x_wins = board.cells.map {|token| board.cells(token).index if token == "X"}
-    binding.pry
+    x_wins = board.cells.map {|token| board.cells(token).index if token == "X" binding.pry}
     WIN_COMBINATIONS.select do |win_option|
-
+      
     end
     board.cells.select d
 
