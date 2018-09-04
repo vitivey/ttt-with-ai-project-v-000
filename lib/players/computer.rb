4863ee305d958a8910e85579c03ab6d0251e822d
Zindzi Thompson <zindzi.thompson@gmail.com>
2018-09-04 02:56:54 +0000

    automatically backed up by learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -7,7 +7,6 @@ module Players
       def move(board)
         array = []
         board.cells.each_with_index {|position, index| array << index+1 if position == " " }
-        binding.pry
         array.sample
       end
 
